3e76d4a7136b67a3f2cec495eb35ddad24db3aaf
a01740729 <a01740729@itesm.mx>
2021-09-07 14:05:50 +0000

    sumanumeros

---
+++ assignments/SumaNumeros/src/exercise.py
@@ -0,0 +1,16 @@
+def main():
+    #escribe tu código abajo de esta línea
+    pass
+
+    suma = 0
+
+    a = int(input('Dame un número: '))
+
+    while a != 0 :
+        suma = suma + a 
+        a = int(input('Dame un número: '))
+    
+    print(suma)
+    
+if __name__=='__main__':
+    main()
